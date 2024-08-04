6dab7f8d3a7146808b732e7b171980af991e31e9
Dhirendra <dhirendragithub@gmail.com>
2024-08-04 19:42:21 +0530

    First commit

---
+++ src/test/java/quantum/org/design_patterns/DesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package quantum.org.design_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
